c2c47bac8e654924182573c20d15701f629c5977
Uacias <filgum0326@gmail.com>
2024-12-04 21:06:56 +0100

    working version.

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct RpcResponse<T> {
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new();
 
-    let rpc_request = RpcRequest {
+    let invoke_req = RpcRequest {
         id: 1,
         jsonrpc: "2.0",
         method: "starknet_traceTransaction",
@@ -32,12 +32,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     };
 
     // Set correct url
-    let url = "";
+    let url = "https://starknet-mainnet.g.alchemy.com/starknet/version/rpc/v0_7/";
     let response = client
         .post(url)
         .header("Accept", "application/json")
         .header("Content-Type", "application/json")
-        .json(&rpc_request)
+        .json(&invoke_req)
         .send()
         .await?;
 
@@ -45,7 +45,100 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let response: RpcResponse<TransactionTrace<Felt>> = response.json().await?;
         println!("{:?}", response);
 
-        let file = File::create("response.json")?;
+        let file = File::create("response_invoke.json")?;
+        let writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(writer, &response)?;
+
+        println!("Response successfully saved to asd.json");
+    } else {
+        let status = response.status();
+        let error_text = response.text().await?;
+        eprintln!("Request failed with status {}: {}", status, error_text);
+    }
+
+    let deploy_acc = RpcRequest {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "starknet_traceTransaction",
+        params: vec!["0x03cda84d48a31f0db50705347108c81a37292098e94d0ba5d9ab52c1568c2928"],
+    };
+
+    let response = client
+        .post(url)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .json(&deploy_acc)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let response: RpcResponse<TransactionTrace<Felt>> = response.json().await?;
+        println!("{:?}", response);
+
+        let file = File::create("response_deploy_acc.json")?;
+        let writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(writer, &response)?;
+
+        println!("Response successfully saved to asd.json");
+    } else {
+        let status = response.status();
+        let error_text = response.text().await?;
+        eprintln!("Request failed with status {}: {}", status, error_text);
+    }
+
+    let declare = RpcRequest {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "starknet_traceTransaction",
+        params: vec!["0x0144949e295dd91d7aa29ca08304b053121ac9f62d7ff43067d5c47d0354855f"],
+    };
+
+    let response = client
+        .post(url)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .json(&declare)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let response: RpcResponse<TransactionTrace<Felt>> = response.json().await?;
+        println!("{:?}", response);
+
+        let file = File::create("response_declare.json")?;
+        let writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(writer, &response)?;
+
+        println!("Response successfully saved to asd.json");
+    } else {
+        let status = response.status();
+        let error_text = response.text().await?;
+        eprintln!("Request failed with status {}: {}", status, error_text);
+    }
+
+    let l1_handler = RpcRequest {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "starknet_traceTransaction",
+        params: vec!["0x030aafc11773e25dff4bbef2d6801f201b474a5685aa7f90c8667ad82f0d2c06"],
+    };
+
+    let response = client
+        .post(url)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .json(&l1_handler)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let response: RpcResponse<TransactionTrace<Felt>> = response.json().await?;
+        println!("{:?}", response);
+
+        let file = File::create("response_l1_handler.json")?;
         let writer = BufWriter::new(file);
 
         serde_json::to_writer_pretty(writer, &response)?;
